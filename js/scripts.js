a98bae5673592031ca3f0f069dbbd4dd440b758e
Tyler Bates <Fondlytyler@gmail.com>
2020-04-10 10:28:40 -0700

    add function to factor size value into price.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,36 @@
 //Business Logic:
 
-function Pizza(size, topping, base, coupon) {
+function Pizza(size, topping, base, coupon, total) {
   this.size = size;
   this.topping = topping;
-  this.base = base; //might not need
+  // this.base = base; //might not need
   this.coupon = coupon;
+  // this.total = total;
 }
 
 //prototype method that uses the properties of the object to determine the price and adds price as a new property to the object
 Pizza.prototype.getPrice = function() {
-  this.base = 8;
+  this.price = 8;
 
-
-
-
+  if (this.size === 'Small') {
+    this.price -= 2;
+  } else if (this.size === 'Large') {
+    this.price += 2; 
+  } else if (this.size === '"tilt to fit through my door" Large') {
+    this.price += 5;
+  } else {
+    return this.price;
+  }
+  
+  // $("input[type=radio]").click(function(this.base) {
+  //   this.base = 0;
+  //   $("input[type=radio]:checked").each(function() {
+  //       this.base += parseInt($(this).val());
+  //   });
+  //   return this.base;
+  
+  // $("#outputnamehere").val(total);
+// });
 }
 
 //User Interface Logic:
@@ -22,6 +39,16 @@ $(document).ready(function() {
   $("form#pizzaSpecs").submit(function(event) {
     event.preventDefault();
     var inputSize = $("#size").val();
-    var inputTopping = $("input:radio:checked").val();
+    var inputTopping = $("input[type=radio]:checked").val();
+    
+    var pizza = new Pizza(inputSize, inputTopping);
+    pizza.getPrice(this.base);
+    $("#priceOutput").html(pizza.price);
+    $(".menu").fadeOut(200);
+    $(".results").fadeIn(1700);
+    console.log(pizza.price);
+    
+    
+    // console.log(inputSize, inputTopping, pizza.base)
   })
-})
+});
