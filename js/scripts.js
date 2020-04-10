f8b175c07a0275f4e09783ffdd7e33965ff61b22
Tyler Bates <Fondlytyler@gmail.com>
2020-04-10 15:14:06 -0700

    Get the checked toppings to add appropriately.

--- js/scripts.js
+++ js/scripts.js
@@ -8,44 +8,44 @@ function Pizza(size, topping, coupon,) {
 }
 
 //prototype method that uses the properties of the object to determine the price and adds price as a new property to the object
-Pizza.prototype.sizePrice = function() {
-  
-  this.price = 8;
-  if (this.size === 'Small') {
-    this.price -= 2;
-  } else if (this.size === 'Large') {
-    this.price += 2; 
-  } else if (this.size === '"tilt to fit through my door" Large') {
-    this.price += 5;
-  } else {
-    return this.price;
-  }
-}
 
 
 
   
 
-  // Pizza.prototype.totalPrice = function() {
-
-  //   this.priceTotal = this.price += this.topPrice;
-  //}
-
+ 
 //User Interface Logic:
 
 $(document).ready(function() {
-  // let topHolder = []
+   let topHolder = []
+    let topping = ''
+  let topPrice
+
+  Pizza.prototype.sizePrice = function() {
   
-  
-  // $("input:checkbox").select(function() {
+    this.price = 8;
+    if (this.size === 'Small') {
+       this.price -= 2;
+    } else if (this.size === 'Large') {
+       this.price += 2; 
+    } else if (this.size === '"tilt to fit through my door" Large') {
+       this.price += 5;
+    } else {
+       this.price;
+    }
+    return topPrice + this.price;
+  }
+
+  $("input:checkbox").click(function() {
     
-  //   topHolder.push(parseInt($(this).val()))
-  //   //Loops through topHolder Array and adds all the values up
-  //   let topPrice = topHolder.reduce(function(accumulator, currentValue){
-  //     return accumulator + currentValue
-  //   })
-  //   console.log(topPrice)
-  // });
+    topHolder.push(parseInt($(this).val()))
+    topping = $(this).attr('id')
+    console.log(topping)
+    //Loops through topHolder Array and adds all the values up
+    topPrice = topHolder.reduce(function(accumulator, currentValue){
+      return accumulator + currentValue
+    })
+  });
 
   $('#button1').on('click', '#start', function(event) {
     event.preventDefault();
@@ -58,13 +58,9 @@ $(document).ready(function() {
     
     
     var inputSize = $("#size").val();
-    var inputTopping = parseInt($("input:checkbox:checked").val());
-    console.log(inputTopping)
-    var pizza = new Pizza(inputSize, inputTopping);
-    pizza.sizePrice();
-    // pizza.toppingPrice();
-    // pizza.totalPrice();
-    $("#priceOutput").html(pizza.price);
+    
+    var pizza = new Pizza(inputSize);
+    $("#priceOutput").html(pizza.sizePrice());
     $(".menu").fadeOut(300);
     $(".results").fadeIn(1700);
 
@@ -73,7 +69,9 @@ $(document).ready(function() {
       $(".results").fadeOut(500);
       $(".menu").fadeIn(1700);
       document.getElementById('pizzaSpecs').reset();
-      // pizza.priceTotal = 0;
+      topPrice = 0;
+      pizza.price = 0;
+      topHolder =[0]
     })
   })
 });
